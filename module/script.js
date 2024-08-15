8fa49ebd9304fcbf81ba980b9a59a3a956ef072d
Andrezzzz421 <andresdavidproxd@gmail.com>
2024-08-15 13:00:38 -0500

    feat: :sparkles: Nuevas funcionalidades para nav

--- module/script.js
+++ module/script.js
@@ -21,3 +21,13 @@ document.addEventListener("DOMContentLoaded", function() {
         navLinks.classList.toggle('active');
     });
 });
+
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {  // Puedes ajustar este valor según prefieras
+        navbar.classList.add('transparent');
+    } else {
+        navbar.classList.remove('transparent');
+    }
+});
+
